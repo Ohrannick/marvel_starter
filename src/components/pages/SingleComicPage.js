32998c5557d4f43fd7411ed1cd2702dddeefd1eb
Ohrannick <ohrannick@gmail.com>
2022-03-25 12:23:08 +0300

    use useNavigate

--- src/components/pages/SingleComicPage.js
+++ src/components/pages/SingleComicPage.js
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 import useMarvelService from '../../servises/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import SingleComic from '../singleComic/SingleComic';
 
 const SingleComicPage = () => {
+  let navigate = useNavigate();
   const { comicId } = useParams();
   const [comic, setComic] = useState(null);
   const { loading, error, getComic, clearError } = useMarvelService();
@@ -16,7 +19,9 @@ const SingleComicPage = () => {
 
   const updateComic = () => {
     clearError();
-    getComic(comicId).then(onComicLoaded);
+    getComic(comicId)
+      .then(onComicLoaded)
+      .catch((err) => (err ? navigate('/404', { replace: true }) : null));
   };
 
   const onComicLoaded = (comic) => {
