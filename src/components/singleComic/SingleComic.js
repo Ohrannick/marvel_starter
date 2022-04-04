2ffc39dc0e9199a9c891a2c75dbbf7e6aa8ae7d6
Ohrannick <ohrannick@gmail.com>
2022-04-04 15:11:14 +0300

    change image not available for comic

--- src/components/singleComic/SingleComic.js
+++ src/components/singleComic/SingleComic.js
@@ -3,10 +3,19 @@ import './singleComic.scss';
 
 const SingleComic = ({ comic }) => {
   const { title, thumbnail, price, description, pageCount, language } = comic;
+  const styleImg = thumbnail.indexOf('not_available') !== -1;
   return (
     <div className='comic'>
       <div className='single-comic'>
-        <img src={thumbnail} alt={title} className='single-comic__img' />
+        <img
+          src={
+            !styleImg
+              ? thumbnail
+              : 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/portrait_xlarge.jpg'
+          }
+          alt={title}
+          className='single-comic__img'
+        />
         <div className='single-comic__info'>
           <h2 className='single-comic__name'>{title}</h2>
           <p className='single-comic__descr'>{description}</p>
